f61afef72dea3700d59eb2ce0c3e29ba6ebe6f21
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-06-11 12:44:03 +0700

    Menambahkan package react-router-dom

--- src/Component/Class/BootstrapComp.jsx
+++ src/Component/Class/BootstrapComp.jsx
@@ -1,20 +1,18 @@
-import React, { Component, useState } from 'react'
-import { Jumbotron, Container, 
-    Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap'
+import React, { Component } from 'react'
+import { Jumbotron, Container} from 'reactstrap'
 import './CSS/BootstrapComp.css'
+//import Navbar1 from './Navbar1'
 
 class BootstrapComp extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-             collapsed: useState,
-             setCollapsed: useState,
-             }
-        
-    }
-    toggleNavbar = () => this.state.setCollapsed(!this.state.collapsed)
 
+        }
+
+    }
+    
     render() {
         return (
 
@@ -28,22 +26,9 @@ class BootstrapComp extends Component {
                         </Container>
                     </Jumbotron>
                 </Container>
-                    <Container>
-                        <Navbar color="faded" light>
-                            <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-                            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                            <Collapse isOpen={!this.state.collapsed} navbar>
-                                <Nav navbar>
-                                    <NavItem>
-                                        <NavLink href="/components/">Components</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                                    </NavItem>
-                                </Nav>
-                            </Collapse>
-                        </Navbar>
-                    </Container>
+                <Container>
+                    
+                </Container>
             </div>
         )
     }
